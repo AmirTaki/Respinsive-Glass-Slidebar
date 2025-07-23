a547757ac9582daf615bf960fcb13496681fcd50
amir <amirtaki623@gmail.com>
2025-07-23 23:12:02 +0330

    searchBtn js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 let btn = document.querySelector("#btn")
 let sidebar = document.querySelector(".sidebar")
+let searchBtn = document.querySelector("bx-search");
 
 
 btn.addEventListener("click", (e)=>{
     sidebar.classList.toggle("active")
 }) 
+
+searchBtn.addEventListener("click", (e)=>{
+    sidebar.classList.toggle("active")
+})
