811e90ef2c71f380be80bd19e92cd3245ad36dc8
amir <amirtaki623@gmail.com>
2025-07-23 23:15:03 +0330

    add js activeLink js at project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 let btn = document.querySelector("#btn")
 let sidebar = document.querySelector(".sidebar")
 let searchBtn = document.querySelector("bx-search");
+let listItem = document.querySelectorAll("list-item");
 
 
 btn.addEventListener("click", (e)=>{
@@ -10,3 +11,18 @@ btn.addEventListener("click", (e)=>{
 searchBtn.addEventListener("click", (e)=>{
     sidebar.classList.toggle("active")
 })
+
+const activeLink = () =>{
+    listItem.forEach((item) => {
+        item.classList.remove("active") 
+        this.classList.add('active')
+
+    })
+ 
+}
+
+   listItem.forEach((item) => {
+        item.addEventListener('click', (e)=>{
+            activeLink()
+        })
+    });
